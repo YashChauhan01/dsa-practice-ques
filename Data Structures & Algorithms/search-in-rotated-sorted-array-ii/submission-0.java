dde7ee0f215ec26649f70a8f537b8bcd479f0e2b
YashChauhan01 <144600769+YashChauhan01@users.noreply.github.com>
2026-07-14 02:15:12 +0530

    Add: search-in-rotated-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int n=nums.length;
+        int l=0,h=n-1;
+        while(l<=h)
+        {
+            int m=l+(h-l)/2;
+            if(target==nums[m])
+            return true;
+            if(nums[l]==nums[m] && l!=m)
+            l++;
+            else if(nums[h]==nums[m] && m!=h)
+            h--;
+            else if(nums[l]<=nums[m]) // left is sorted
+            {
+                if(target>=nums[l] && nums[m]>target)
+                h=m-1;
+                else
+                l=m+1;
+            }
+            else // right is sorted
+            {
+                if(target>nums[m] && nums[h]>=target)
+                l=m+1;
+                
+                else
+                h=m-1;
+            }
+        }
+        return false;
+    }
+}
